c50221d25c7053b9df302ceedc31d9b7b53fe330
Arturo Villa <al222753@alumnos.uacj.mx>
2024-10-29 10:56:15 -0600

    Se agrego la funcion de cajero automatico en Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -5,17 +5,60 @@
 using namespace std;
 
 int factorial(int n);
+void consulta(int s);
+int retiro(int s);
 
 int result;
 
 int main() {
 
-    int N;
+    int N, opcion, A, saldo = 20000;
 
-    cout << "\nCual numero desea conseguir su factorial? -->";
-    cin >> N;
+    cout << "\nCual programa desea ejecutar? -->";
+    cout << "\n1- Cajero automatico";
+    cout << "\n2- Factorial de N";
+    cout << "\nCual elije? --> ";
+    cin >> opcion;
 
-    cout << "\nLa factorial es: " << factorial(N) << endl;
+    cout << endl;
+
+    switch (opcion) {
+        case 1:
+
+            do {
+                cout << "\n\tCajero automatico";
+                cout << "\n1- Consultar saldo";
+                cout << "\n2- Retirar dinero";
+                cout << "\n3- Salir";
+                cout << "\nCual elije? --> ";
+                cin >> A;
+
+                switch (A) {
+                    case 1:
+                        consulta(saldo);
+                        break;
+                    case 2:
+                        saldo = retiro(saldo);
+                        break;
+                    case 3:
+                        cout << "\nSe termino el programa, hasta luego";
+                    break;
+                    default:
+                        cout << "\nEsa opcion no existe, ingrese otra";
+                    break;
+                }
+            }while (A != 3);
+
+            break;
+        case 2:
+            cout << "\nCual numero desea conseguir su factorial? -->";
+            cin >> N;
+
+            cout << "\nLa factorial es: " << factorial(N) << endl;
+            break;
+        default:
+            break;
+    }
 
     return 0;
 }
@@ -30,3 +73,22 @@ int factorial(int n) {
     return result;
 
 }
+
+void consulta(int s) {
+
+    cout << "\nSu saldo total es: " << s;
+
+}
+
+int retiro(int s) {
+
+    int r;
+
+    cout << "\nCuanto desea retira? --> ";
+    cin >> r;
+
+    cout << "\nSe a retirado un total de: " << r << ". Su saldo actual es de: " << s - r;
+
+    return (s - r);
+}
+
